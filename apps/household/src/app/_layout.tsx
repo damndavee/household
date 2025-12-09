83451a95b42299bb44c8cac7429924f87fca1754
Pawel Kopij <pawel.kopij@capgemini.com>
2025-12-09 13:47:58 +0100

    add react native lottie

--- apps/household/src/app/_layout.tsx
+++ apps/household/src/app/_layout.tsx
@@ -3,6 +3,14 @@ import '../../global.css';
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import * as SplashScreen from 'expo-splash-screen';
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
 const RootLayout = () => {
   return (
     <SafeAreaProvider>
--- apps/household/src/app/index.tsx
+++ apps/household/src/app/index.tsx
@@ -1,12 +1,30 @@
-import { Text, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Animated, Easing, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+const AnimatedLottieView = Animated.createAnimatedComponent(LottieView);
 
 const RootPage = () => {
+  const animationProgress = useRef(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(animationProgress.current, {
+      toValue: 1,
+      duration: 5000,
+      easing: Easing.linear,
+      useNativeDriver: false,
+    }).start();
+  }, []);
+
   return (
-    <View className="justify-center items-center flex-1 gap-4">
-      <View className="border-2 rounded-[5] p-4">
-        <Text>Hello from inner View</Text>
-      </View>
-      <Text className="color-amber-600">Hello from Expo Router!@#</Text>
+    <View className="justify-center items-center flex-1 gap-4 bg-[#FFFCF0]">
+      <AnimatedLottieView
+        source={require('../../assets/animations/solar_powered_house.json')}
+        progress={animationProgress.current}
+        style={{ width: '100%', height: '100%' }}
+        autoPlay={true}
+        loop={true}
+      />
     </View>
   );
 };
